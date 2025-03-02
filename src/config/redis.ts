201d4fd0eb7dacc1eaa6c0f2c0cbdd488ec2d635
Abhishek Jha <abhishek.jha@wolfmatrix.com>
2025-03-02 20:39:53 +0545

    add password in redis client

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const redisClient: RedisClientType = createClient({
   url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD || undefined,
 });
 
 redisClient.on("error", (err) => console.log("Redis client error", err));
